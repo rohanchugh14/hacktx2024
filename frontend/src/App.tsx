94f9c16ffb9567263f93c0b7c3070dcd8e275c94
rohanchugh14 <rohanchugh14@gmail.com>
2024-11-02 20:18:18 -0500

    update frontend and add some requests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+// make a request with axios, get request
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = (await axios.get('https://api.usaspending.gov/api/v2/references/toptier_agencies/?sort=percentage_of_total_budget_authority&order=desc')).data.results
+      console.log(res)
+      const awardInfo = (await axios.get(`https://api.usaspending.gov/api/v2/agency/${res[0].toptier_code}/budgetary_resources/`)).data
+      console.log(awardInfo)
+    }
+    fetchData()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hi there
     </div>
   );
 }
